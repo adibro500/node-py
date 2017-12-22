196c9d045d0d951c4b8c87d4afc69f8075f1ad0d
Aditya Ammanabrolu <ammanabrolu.aditya@gmail.com>
2017-12-22 11:57:48 +0530

    comm

---
+++ constants/api-constants.js
@@ -0,0 +1,7 @@
+module.exports = {
+POST_LOGIN_API : "/api/login/",
+POST_ADMIN_INPUT_MASTER : "/saveTemplate-adminInputMaster",
+GET_INPUT_MASTER : "/getTemplate",
+POST_FORM_ADMIN_INPUT_MASTER : "/save/admin-input-master",
+SECRET_KEY:'alto'
+};
--- index.js
+++ index.js
@@ -4,8 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var dbCons = require("./constants/db-constants");
+var apiCons = require("./constants/api-constants");
 var cors = require('cors');
+var multer = require("multer");
+
 
 var app = express();
 
@@ -20,9 +23,9 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -36,7 +39,7 @@ res.send("welcome to express");
 
 var db;
 //mongodb://<dbuser>:<dbpassword>@ds133378.mlab.com:33378/kleveron
-MongoClient.connect('mongodb://root:root@ds133378.mlab.com:33378/kleveron', (err, database) => {
+MongoClient.connect(dbCons.MONGO_URL, (err, database) => {
   if (err) return console.log(err)
   else{
     console.log("db done.....");
@@ -46,7 +49,7 @@ MongoClient.connect('mongodb://root:root@ds133378.mlab.com:33378/kleveron', (err
  
 })
 
-app.post('/api/login/', function(req, res, err) {
+app.post(apiCons.POST_LOGIN_API, function(req, res, err) {
   db.collection('logins').findOne({ username: req.body.username}, function(err, user) {
     console.log('User found ',req.body);
     // In case the user not found  
@@ -56,10 +59,10 @@ app.post('/api/login/', function(req, res, err) {
     } 
     if (user!=null && (user.password === req.body.password)){
       console.log('User and password is correct');
-      res.json(user);
+      res.json({"data":"valid"});
     } else {
       console.log("Credentials wrong");
-      res.json({data: "Login invalid"});
+      res.json({"data":"invalid"});
     }              
 });
 })
@@ -80,6 +83,197 @@ app.post('/api/login/', function(req, res, err) {
 //     }
 //   })
 // })
+app.post(apiCons.POST_ADMIN_INPUT_MASTER,function(req,res){
+  console.log("innnnn");
+  db.collection('adminInputMasterTemplates').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+else    
+    console.log('saved to database')
+    
+  })
+
+
+});
+
+app.get(apiCons.GET_INPUT_MASTER, function(req, res, err) {
+  db.collection('adminInputMasterTemplates').find({ "user": "ravi"}).toArray( function(err, users) {
+    console.log('User found ',req.body);
+    // In case the user not found  
+    if(err) {
+      console.log('THIS IS ERROR RESPONSE');
+      res.json(err);
+    } 
+    // if (user!=null && (user.password === req.body.password)){
+    //   console.log('User and password is correct');
+    //   res.json(user);
+    // }
+     else {
+      console.log("Credentials wrong");
+      res.send(users);
+    }              
+});
+})
+
+app.post("/save/admin-list-master",function(req,res){
+  console.log("innnnn");
+  db.collection('saveGridMaster').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+else    
+    console.log('saved to database')
+    
+  })
+
+
+});
+
+
+app.get("/get/admin-grid-master", function(req, res, err) {
+  db.collection('saveGridMaster').find({ "user": "ravi"}).toArray( function(err, users) {
+    console.log('User found ',req.body);
+    // In case the user not found  
+    if(err) {
+      console.log('THIS IS ERROR RESPONSE');
+      res.json(err);
+    } 
+    // if (user!=null && (user.password === req.body.password)){
+    //   console.log('User and password is correct');
+    //   res.json(user);
+    // }
+     else {
+      console.log("Credentials wrong");
+      res.send(users);
+    }              
+});
+})
+
+
+
+//POST_FORM_ADMIN_INPUT_MASTER
+app.post(apiCons.POST_FORM_ADMIN_INPUT_MASTER,function(req,res){
+  console.log("innnnn");
+  db.collection('adminInputMaster_form_inputs').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+else    
+    console.log('saved to database')
+    
+  })
+
+
+});
+
+app.post("/save/admin2",function(req,res){
+  console.log("innnnn");
+  db.collection('admin2inputs').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+else    
+    console.log('saved to database')
+    
+  })
+
+
+});
+
+app.post("/save/admin-input2",function(req,res){
+  console.log("innnnn");
+  db.collection('admin2').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+else    
+    console.log('saved to database')
+    
+  })
+
+
+});
+
+
+
+// app.post("/savelist/adminInputMaster",function(req,res){
+//   console.log("innnnn");
+//   db.collection('adminInputMasterTemplate_list').save(req.body, (err, result) => {
+//     if (err) return console.log(err)
+// else    
+//     console.log('saved to database')
+    
+//   })
+
+
+// });
+
+
+app.get("/getlist/admin-input-master",function(req,res){
+  db.collection('adminInputMasterTemplate_list').find({ "user": "ravi"}).toArray( function(err, users) {
+    console.log('User found ',req.body);
+    // In case the user not found  
+    if(err) {
+      console.log('THIS IS ERROR RESPONSE');
+      res.json(err);
+    } 
+   
+     else {
+      console.log("Credentials wrong");
+      res.send(users);
+    }
+                  
+});
+
+
+});
+
+app.get("/get/admin2",function(req,res){
+  db.collection('admin2').find({ "User": "Aditya"}).toArray( function(err, users) {
+    console.log('User found ',req.body);
+    // In case the user not found  
+    if(err) {
+      console.log('THIS IS ERROR RESPONSE');
+      res.json(err);
+    } 
+   
+     else {
+      console.log("Credentials wrong");
+      res.send(users);
+    }
+                  
+});
+
+
+});
+
+// var DIR = './uploads/';
+
+// var upload = multer({dest: DIR});
+
+
+
+// app.use(multer({
+//  dest: DIR,
+//  rename: function (fieldname, filename) {
+//    return filename + Date.now();
+//  },
+//  onFileUploadStart: function (file) {
+//    console.log(file.originalname + ' is starting ...');
+//  },
+//  onFileUploadComplete: function (file) {
+//    console.log(file.fieldname + ' uploaded to  ' + file.path);
+//  }
+// }));
+
+// app.get('/upload1', function (req, res) {
+//  res.end('file catcher example');
+// });
+
+// app.post('/upload2', function (req, res) {
+//  upload(req, res, function (err) {
+//    if (err) {
+//      return res.end(err.toString());
+//    }
+
+//    res.end('File is uploaded');
+//  });
+// });
+
+
+
+
 
 
 app.use(function(req, res, next) {
