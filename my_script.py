6d9ef7464332c1cc536490bec52aaacd69d58b0d
aditya <ammanabrolu.aditya@gmail.com>
2018-01-31 18:55:18 +0530

    comm

--- my_script.py
+++ my_script.py
@@ -1,11 +1,3 @@
-import numpy as np
-
-a = np.array([1, 2, 3])   # Create a rank 1 array
-print('\n',type(a))            # Prints "<class 'numpy.ndarray'>"
-print('\n',a.shape)            # Prints "(3,)"
-print('\n',a[0], a[1], a[2])   # Prints "1 2 3"
-a[0] = 5                  # Change an element of the array
-print('\n',a)                  # Prints "[5, 2, 3]"
-b = np.array([[1,2,3],[4,5,6]])    # Create a rank 2 array
-print('\n',b.shape)    # Prints "(2, 3)"
-print('\n',b[0, 0], b[0, 1], b[1, 0])   # Prints "1 2 4"
+import matplotlib.pyplot as plt, mpld3
+plt.plot([3,1,4,1,5], 'ks-', mec='w', mew=5, ms=20)
+mpld3.show()
